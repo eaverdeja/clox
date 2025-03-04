279c4038757b37cb2241e5d8ecb368587c361263
eaverdeja <eaverdeja@gmail.com>
2025-03-04 09:02:46 -0300

    14.5 - Constant values
    https://craftinginterpreters.com/chunks-of-bytecode.html#constants

--- src/chunk.c
+++ src/chunk.c
@@ -2,17 +2,20 @@
 
 #include "chunk.h"
 #include "memory.h"
+#include "value.h"
 
 void initChunk(Chunk *chunk)
 {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk *chunk)
 {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
 
@@ -27,4 +30,10 @@ void writeChunk(Chunk *chunk, u_int8_t byte)
 
     chunk->code[chunk->count] = byte;
     chunk->count++;
+}
+
+int addConstant(Chunk *chunk, Value value)
+{
+    writeValueArray(&chunk->constants, value);
+    return chunk->constants.count - 1;
 }
