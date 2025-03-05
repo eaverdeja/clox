293c7ac59e31a30d8a4b6ad219ce2696c51a7cf3
eaverdeja <eaverdeja@gmail.com>
2025-03-05 10:11:13 -0300

    17 . 6 - A Pratt Parser
    https://craftinginterpreters.com/compiling-expressions.html#a-pratt-parser

--- src/compiler.c
+++ src/compiler.c
@@ -6,6 +6,9 @@
 #include "chunk.h"
 #include "common.h"
 #include "scanner.h"
+#ifdef DEBUG_PRINT_CODE
+#include "debug.h"
+#endif
 
 typedef struct {
     Token current;
@@ -28,7 +31,11 @@ typedef enum {
     PREC_PRIMARY,
 } Precedence;
 
+typedef void (*ParseFn)();
+
 typedef struct {
+    ParseFn prefix;
+    ParseFn infix;
     Precedence precedence;
 } ParseRule;
 
@@ -104,10 +111,45 @@ static void emitConstant(Value value) {
     emitBytes(OP_CONSTANT, makeConstant(value));
 }
 
-static void endCompiler() { emitReturn(); }
+static void endCompiler() {
+    emitReturn();
+#ifdef DEBUG_PRINT_CODE
+    if (!parser.hadError) {
+        dissasembleChunk(currentChunk(), "code");
+    }
+#endif
+}
 
-static void parsePrecedence(Precedence precedence) {
-    // What goes here?
+static void expression();
+static ParseRule* getRule(TokenType type);
+static void parsePrecedence(Precedence precedence);
+
+static void binary() {
+    TokenType operatorType = parser.previous.type;
+    ParseRule* rule = getRule(operatorType);
+
+    // The "+1" approach ensures that each operator's right-hand side is parsed
+    // at a slightly higher precedence level, preventing lower-precedence
+    // operators from "stealing" parts of the expression they shouldn't.
+    // Ex. 1 + 2 * 3 the '+' shouldn't steal the '* 3' part of the expression.
+    parsePrecedence((Precedence)rule->precedence + 1);
+
+    switch (operatorType) {
+        case TOKEN_PLUS:
+            emitByte(OP_ADD);
+            break;
+        case TOKEN_MINUS:
+            emitByte(OP_SUBTRACT);
+            break;
+        case TOKEN_STAR:
+            emitByte(OP_MULTIPLY);
+            break;
+        case TOKEN_SLASH:
+            emitByte(OP_DIVIDE);
+            break;
+        default:
+            break;
+    }
 }
 
 static void expression() { parsePrecedence(PREC_ASSIGNMENT); }
@@ -138,33 +180,67 @@ static void unary() {
     }
 }
 
-static void binary() {
-    TokenType operatorType = parser.previous.type;
-    ParseRule* rule = getRule(operatorType);
+ParseRule rules[] = {
+    [TOKEN_LEFT_PAREN] = {grouping, NULL, PREC_NONE},
+    [TOKEN_RIGHT_PAREN] = {NULL, NULL, PREC_NONE},
+    [TOKEN_LEFT_BRACE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_RIGHT_BRACE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_COMMA] = {NULL, NULL, PREC_NONE},
+    [TOKEN_DOT] = {NULL, NULL, PREC_NONE},
+    [TOKEN_MINUS] = {unary, binary, PREC_TERM},
+    [TOKEN_PLUS] = {NULL, binary, PREC_TERM},
+    [TOKEN_SEMICOLON] = {NULL, NULL, PREC_NONE},
+    [TOKEN_SLASH] = {NULL, binary, PREC_FACTOR},
+    [TOKEN_STAR] = {NULL, binary, PREC_FACTOR},
+    [TOKEN_BANG] = {NULL, NULL, PREC_NONE},
+    [TOKEN_BANG_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_EQUAL_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_GREATER] = {NULL, NULL, PREC_NONE},
+    [TOKEN_GREATER_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_LESS] = {NULL, NULL, PREC_NONE},
+    [TOKEN_LESS_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_IDENTIFIER] = {NULL, NULL, PREC_NONE},
+    [TOKEN_STRING] = {NULL, NULL, PREC_NONE},
+    [TOKEN_NUMBER] = {number, NULL, PREC_NONE},
+    [TOKEN_AND] = {NULL, NULL, PREC_NONE},
+    [TOKEN_CLASS] = {NULL, NULL, PREC_NONE},
+    [TOKEN_ELSE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FALSE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FOR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FUN] = {NULL, NULL, PREC_NONE},
+    [TOKEN_IF] = {NULL, NULL, PREC_NONE},
+    [TOKEN_NIL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_OR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_PRINT] = {NULL, NULL, PREC_NONE},
+    [TOKEN_RETURN] = {NULL, NULL, PREC_NONE},
+    [TOKEN_SUPER] = {NULL, NULL, PREC_NONE},
+    [TOKEN_THIS] = {NULL, NULL, PREC_NONE},
+    [TOKEN_TRUE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_VAR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_WHILE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_ERROR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_EOF] = {NULL, NULL, PREC_NONE},
+};
 
-    // The "+1" approach ensures that each operator's right-hand side is parsed
-    // at a slightly higher precedence level, preventing lower-precedence
-    // operators from "stealing" parts of the expression they shouldn't.
-    // Ex. 1 + 2 * 3 the '+' shouldn't steal the '* 3' part of the expression.
-    parsePrecedence((Precedence)rule->precedence + 1);
+static void parsePrecedence(Precedence precedence) {
+    advance();
+    ParseFn prefixRule = getRule(parser.previous.type)->prefix;
+    if (prefixRule == NULL) {
+        error("Expect expression.");
+        return;
+    }
 
-    switch (operatorType) {
-        case TOKEN_PLUS:
-            emitByte(OP_ADD);
-            break;
-        case TOKEN_MINUS:
-            emitByte(OP_SUBTRACT);
-            break;
-        case TOKEN_STAR:
-            emitByte(OP_MULTIPLY);
-            break;
-        case TOKEN_SLASH:
-            emitByte(OP_DIVIDE);
-            break;
-        default:
-            break;
+    prefixRule();
+
+    while (precedence <= getRule(parser.current.type)->precedence) {
+        advance();
+        ParseFn infixRule = getRule(parser.previous.type)->infix;
+        infixRule();
     }
 }
+
+static ParseRule* getRule(TokenType type) { return &rules[type]; }
 
 bool compile(const char* source, Chunk* chunk) {
     initScanner(source);
