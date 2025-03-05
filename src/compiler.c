ec41f5610d9596f958697ac045934a9e69352cca
eaverdeja <eaverdeja@gmail.com>
2025-03-05 08:10:11 -0300

    17.4 - Parsing Prefix Expressions
    https://craftinginterpreters.com/compiling-expressions.html#parsing-prefix-expressions

--- src/compiler.c
+++ src/compiler.c
@@ -14,6 +14,20 @@ typedef struct {
     bool panicMode;
 } Parser;
 
+typedef enum {
+    PREC_NONE,
+    PREC_ASSIGNMENT,  // =
+    PREC_OR,          // or
+    PREC_AND,         // and
+    PREC_EQUALITY,    // == !=
+    PREC_COMPARISON,  // < > <= >=
+    PREC_TERM,        // + -
+    PREC_FACTOR,      // * /
+    PREC_UNARY,       // ! -
+    PREC_CALL,        // . ()
+    PREC_PRIMARY,
+} Precedence;
+
 Parser parser;
 Chunk* compilingChunk;
 
@@ -73,7 +87,52 @@ static void emitBytes(u_int8_t byte1, u_int8_t byte2) {
 
 static void emitReturn() { emitByte(OP_RETURN); }
 
+static uint8_t makeConstant(Value value) {
+    int constant = addConstant(currentChunk(), value);
+    if (constant > UINT8_MAX) {
+        error("Too many constants in one chunk.");
+        return 0;
+    }
+    return (uint8_t)constant;
+}
+
+static void emitConstant(Value value) {
+    emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
 static void endCompiler() { emitReturn(); }
+
+static void parsePrecedence(Precedence precedence) {
+    // What goes here?
+}
+
+static void expression() { parsePrecedence(PREC_ASSIGNMENT); }
+
+static void grouping() {
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void number() {
+    double value = strtod(parser.previous.start, NULL);
+    emitConstant(value);
+}
+
+static void unary() {
+    TokenType operatorType = parser.previous.type;
+
+    // Compile the operand.
+    parsePrecedence(PREC_UNARY);
+
+    switch (operatorType) {
+        case TOKEN_MINUS:
+            emitByte(OP_NEGATE);
+            break;
+        default:
+            // Unreachable
+            return;
+    }
+}
 
 bool compile(const char* source, Chunk* chunk) {
     initScanner(source);
@@ -83,7 +142,7 @@ bool compile(const char* source, Chunk* chunk) {
     parser.panicMode = false;
 
     advance();
-    // expression();
+    expression();
     consume(TOKEN_EOF, "Expect end of expression.");
     endCompiler();
     return !parser.hadError;
