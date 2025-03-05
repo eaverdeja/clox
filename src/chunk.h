5d1b2335a7885ccc45d69dc70658ab21541579f7
eaverdeja <eaverdeja@gmail.com>
2025-03-05 16:08:24 -0300

    18.4 - Two New Types (and comparisons!*)
    https://craftinginterpreters.com/types-of-values.html#two-new-types

--- src/chunk.h
+++ src/chunk.h
@@ -6,6 +6,13 @@
 
 typedef enum {
     OP_CONSTANT,
+    OP_NIL,
+    OP_TRUE,
+    OP_FALSE,
+    OP_NOT,
+    OP_EQUAL,
+    OP_GREATER,
+    OP_LESS,
     OP_ADD,
     OP_SUBTRACT,
     OP_MULTIPLY,
