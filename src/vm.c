e001f24085047228fd12e7f13a4c040d23a87fc4
eaverdeja <eaverdeja@gmail.com>
2025-03-08 12:18:19 -0300

    21.3 - Reading Variables
    https://craftinginterpreters.com/global-variables.html#reading-variables

--- src/vm.c
+++ src/vm.c
@@ -124,6 +124,16 @@ static InterpretResult run() {
             case OP_POP:
                 pop();
                 break;
+            case OP_GET_GLOBAL: {
+                ObjString* name = READ_STRING();
+                Value value;
+                if (!tableGet(&vm.globals, name, &value)) {
+                    runtimeError("Undefined variable '%s'.", name->chars);
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                push(value);
+                break;
+            }
             case OP_DEFINE_GLOBAL: {
                 ObjString* name = READ_STRING();
                 // We don’t pop the value until after we add it to the hash
