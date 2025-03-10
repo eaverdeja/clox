5f8edaae95786806d623fa482762b876ee551bda
eaverdeja <eaverdeja@gmail.com>
2025-03-10 18:49:58 -0300

    23.4 - For Statements
    https://craftinginterpreters.com/jumping-back-and-forth.html#while-statements

--- src/compiler.c
+++ src/compiler.c
@@ -376,8 +376,65 @@ static void varDeclaration() {
 
 static void expressionStatement() {
     expression();
-    consume(TOKEN_SEMICOLON, "Expected ';' after expression.");
+    consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
     emitByte(OP_POP);
+}
+
+static void forStatement() {
+    beginScope();
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+
+    // Initializer
+    if (match(TOKEN_SEMICOLON)) {
+        // No initializer
+    } else if (match(TOKEN_VAR)) {
+        varDeclaration();
+    } else {
+        expressionStatement();
+    }
+
+    int loopStart = currentChunk()->count;
+
+    // Condition
+    int exitJump = -1;
+    if (!match(TOKEN_SEMICOLON)) {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after loop condition");
+
+        // Jump out of the loop if the condition is false.
+        exitJump = emitJump(OP_JUMP_IF_FALSE);
+        emitByte(OP_POP);  // Pop condition.
+    }
+
+    // Increment
+    if (!match(TOKEN_RIGHT_PAREN)) {
+        int bodyJump = emitJump(OP_JUMP);
+        int incrementStart = currentChunk()->count;
+        expression();
+        emitByte(OP_POP);  // Pop increment result.
+        consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        // This is the main loop that takes us back to the top of the for
+        // loop—right before the condition expression
+        emitLoop(loopStart);
+        // We change loopStart to point to the offset where the increment
+        // expression begins. Later, when we emit the loop instruction after the
+        // body statement, this will cause it to jump up to the increment
+        // expression instead of the top of the loop like it does when there is
+        // no increment.
+        loopStart = incrementStart;
+        patchJump(bodyJump);
+    }
+
+    statement();
+    emitLoop(loopStart);
+
+    if (exitJump != -1) {
+        patchJump(exitJump);
+        emitByte(OP_POP);  // Pop condition.
+    }
+
+    endScope();
 }
 
 static void ifStatement() {
@@ -458,6 +515,8 @@ static void declaration() {
 static void statement() {
     if (match(TOKEN_PRINT)) {
         printStatement();
+    } else if (match(TOKEN_FOR)) {
+        forStatement();
     } else if (match(TOKEN_IF)) {
         ifStatement();
     } else if (match(TOKEN_WHILE)) {
